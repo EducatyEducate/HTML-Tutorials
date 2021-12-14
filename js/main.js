071c27e6bc84020d128eabb6050cd093776d96f2
gabzin <anonplayers@gmail.com>
2021-12-14 09:57:10 -0300

    Responsive/Animated

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var element1 = document.getElementById("lesson-text");
+var element2 = document.getElementById("lesson-text2");
+element1.classList.add("animate__animated")
+element1.classList.add("animate__fadeInRight")
+element2.classList.add("animate__animated")
+element2.classList.add("animate__fadeIn")
